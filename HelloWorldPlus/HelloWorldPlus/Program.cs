b7a3ddf7465dc3e0de87968420ead0181322a566
Søren Magnusson <soren@magnussons.dk>
2018-05-17 14:20:15 +0200

    Nu med løsning

--- HelloWorldPlus/HelloWorldPlus/Program.cs
+++ HelloWorldPlus/HelloWorldPlus/Program.cs
@@ -16,16 +16,44 @@ namespace HelloWorldPlus
             Console.WriteLine("Hello World!");
 
             // øv 6
+            Console.WriteLine("Søren Magnusson");
+
+            // øv 6 version 2
+            string fornavn = "Søren";
+            string efternavn = "Magnusson";
+            Console.WriteLine(fornavn + " " + efternavn);
+
+            // øv 6 version 3
+            Console.WriteLine("{0} {1}", fornavn, efternavn);
 
             // øv 7
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
 
             // øv 8
+            Console.WriteLine(DateTime.Now);
             
             // øv 9
+            Console.WriteLine(Math.Sqrt(12345));
 
             // øv 10
+            for (int i = 2; i < 102; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(-i);
+                }
+            }
 
             // øv 11
+            Console.Write("Skriv venligst din alder: ");
+            double alder = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Om 10 år er du nok {0} år", alder + 10);
         }
     }
 }
